ba6709d4a1fa732e9e35c1c0a503bb8cdaf6cd77
Gustavo Souza <gustavo.souza@ba.senac.br>
2026-09-11 15:29:10 -0300

    docs: adicionando exercicicos de pratica

---
+++ praticaLiveCoding/exercicios01/exercicios01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace exercicios01;
+
+class Program
+{
+    
+    public static string FirstReverse(string str)
+    {
+        string reverse = "";
+
+        for(int i = str.Length -1; i >= 0; i--)
+        {
+            reverse += str[i];
+        }
+
+        return reverse;
+    }
+    
+    
+    
+    
+    
+    static void Main()
+    {
+        Console.WriteLine(FirstReverse("Coderbyte"));
+    }
+
+
+}
